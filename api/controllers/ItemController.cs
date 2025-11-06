518b4cc99553714d5e8ba009f46eb5809e2ee618
joegamal <yusufmagamalth@gmail.com>
2025-11-07 01:46:20 +0200

    new info

--- api/controllers/ItemController.cs
+++ api/controllers/ItemController.cs
@@ -6,10 +6,39 @@ namespace api.controllers
     [Route("[item]")]
     public class ItemController : ControllerBase
     {
+        //read
         [HttpGet("all")]
-        public ActionResult GetAll()
+        public IActionResult GetAll()
         {
             return Ok("all data");
         }
+        //create
+        [HttpPost("create/{val}")]
+        public IActionResult Create(string val)
+        {
+            return Ok($"created {val}");
+        }
+
+        //update
+        [HttpPut("update/{val}")]
+        public IActionResult Update(string val)
+        {
+            return Ok($"updated {val}");
+        }
+
+        //delete
+        [HttpDelete("delete/{val}")]
+        public IActionResult Delete(string val)
+        {
+            return Ok($"Deletee {val}");
+        }
+
     }
 }
+
+/**
+the application depends on the dmain 
+the infrastructure depends on the application and the domain
+the presentation depends on all of them 
+the interfaces and dtos and mappers and query handlers and validators are all in application layer
+*/
--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5be42fdb27029e1f62b13efbd09f649e9ee29f88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f8183abd1f3e33e03fc07aeec6e95fa9066b5c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
